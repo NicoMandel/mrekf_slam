a2db5f18ee537cbfe9b30cd3e3ef9b4f9e472b32
Nico Mandel <mandel.nicolas@gmail.com>
2023-08-01 13:30:58 +0200

    added basic equations for Bodyframe model

--- src/mrekf/motionmodels.py
+++ src/mrekf/motionmodels.py
@@ -114,3 +114,67 @@ class KinematicModel(BaseModel):
     def f(self, x : np.ndarray) -> np.ndarray:
         fx_k = self.A @ x
         return fx_k   
+
+class BodyFrame(BaseModel):
+    """
+        Class to provide a Body Frame model, similar to the bicycle model used by PC. However, we include v and omega in the state.
+        So the state is:
+            - x
+            - y
+            - v
+            - omega # todo - check omega has all the base_wrap variables
+        changing the state update equations!
+    """
+
+    def __init__(self, V : np.ndarray, dt : float, vmax : float = 3.0):
+        assert V.shape == (4,4), "V not correct shape, Please make sure it's 4x4"
+        assert np.allclose(V[:2,:2], np.zeros((2,2))), "The top left corner of V should be all zeros, please correct"
+        dim = 4
+
+        Fv = None
+        Fx = None
+        super().__init__(V, Fv, Fx, dim)
+        
+
+    @property
+    def dt(self) -> float:
+        return self._dt
+
+    @property
+    def vmax(self) -> float:
+        return self._vmax
+
+    def fx(self, x: np.ndarray) -> np.ndarray:
+        """
+            Model to predict forward
+        """
+        x_k = np.array([
+            x[0] + self.dt * x[2] * np.cos(x[3]),
+            x[1] + self.dt * x[2] * np.sin(x[3]), 
+            x[2],
+            x[3]
+        ])
+        return x_k
+    
+    def Fx(self, x : np.ndarray) -> np.ndarray:
+        """
+            Since these are nonlinear models, the update equations are different.
+        """
+        fx = np.asarray([#
+            [1., 0., self.dt * np.cos(x[3] + self.V[3,3]),  -1. * self.dt * (x[2] + self.V[2,2]) * np.sin(x[3] + self.V[3,3])],
+            [0., 1., self.dt * np.sin(x[3], self.V[3,3]),   self.dt * (x[2] * self.V[2,2]) * np.cos(x[3] * self.V[3,3])],
+            [0., 0., 1., 0.],
+            [0., 0., 0., 1.]
+            ])
+        return fx
+
+    def Fv(self, x: np.ndarray) -> np.ndarray:
+        fv = np.asarray([#
+            [1., 0., self.dt * np.cos(x[3] + self.V[3,3]),  -1. * self.dt * (x[2] + self.V[2,2]) * np.sin(x[3] + self.V[3,3])],
+            [0., 1., self.dt * np.sin(x[3], self.V[3,3]),   self.dt * (x[2] * self.V[2,2]) * np.cos(x[3] * self.V[3,3])],
+            [0., 0., 1., 0.],
+            [0., 0., 0., 1.]
+            ])
+        return fv
+
+
