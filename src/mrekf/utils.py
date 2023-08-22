72d8c5dfdf192a0f495ff5ead0e7602964d4e2ac
Nico Mandel <mandel.nicolas@gmail.com>
2023-08-22 15:46:15 +0200

    using pickle for writing and loading histories

--- src/mrekf/utils.py
+++ src/mrekf/utils.py
@@ -15,25 +15,7 @@ import numpy as np
 import os.path
 from datetime import date, datetime
 import json
-import h5py
 import pickle
-
-# Section on dumping files
-def dump_json(somedict : dict, fpath):
-    with open(fpath, "w") as outf:
-        json.dump(somedict, outf, cls=NumpyEncoder)
-    return None
-
-def dump_json_nice(somedict : dict, fpath):
-    with open(fpath, "w") as outf:
-        json.dump(somedict, outf, cls=NumpyEncoderNice)
-    return None
-
-def load_json_nice(fpath) -> dict:
-    with open(fpath, 'r') as f:
-        res = json.load(f, object_hook=json_np_obj_hook)
-    return res
-
 
 def convert_experiment_to_dict(somedict : dict) -> dict:
     """
@@ -71,7 +53,7 @@ def get_map_values(lm_map) -> dict:
     lmd = {}
     lmd['workspace'] = lm_map.workspace
     lmd['num_lms'] = lm_map._nlandmarks
-    lmd['landmarks'] = lm_map.landmarks.T
+    lmd['landmarks'] = lm_map.landmarks
     return lmd
 
 def get_robs_values(robots : list, robot_offset : int =0) -> dict:
@@ -121,26 +103,6 @@ def _change_filename(fname : str) -> str:
     fnm = fname + app
     return fnm
 
-def dump_namedtuple(nt : list, dirname : str) -> None:
-    """
-        Function to dump list of namedtuples to json
-    """
-    # first step is to convert to dictionary -> by timestamp
-    outdict = {h.t : h._asdict() for h in nt}
-    
-    # the name is the name of the history
-    hname = type(nt[0]).__name__
-
-    if not os.path.isdir(dirname):
-        print("{} does not exist. Creating".format(dirname))
-        _create_dir(dirname)
-    
-    # save the dictionary
-    outf = os.path.join(dirname, hname + ".json")
-    with open(outf, "w") as outfile:
-        json.dump(outdict, outfile, cls=NumpyEncoderNice)
-    print("Written {} to {}".format(hname, outf))
-
 def _create_dir(dirname : str) -> None:
     """
         Function to create a directory in a parentdir
@@ -149,29 +111,21 @@ def _create_dir(dirname : str) -> None:
     os.makedirs(dirname)
 
 # Section on Loading arrays
-def load_experiment(json_path : str) -> dict:
+def dump_json(exp_dict, fpath):
+    """
+        function to dump the json object
+    """
+    with open(fpath, 'w') as jsf:
+        json.dump(exp_dict, jsf, cls=NumpyEncoder)
+    return None
+
+def load_json(json_path : str) -> dict:
     """
         Function to load experiments from json
     """
     with open(json_path, 'r') as jsf:
-        jsd = json.load(jsf, object_hook=json_list_np_obj_hook)
+        jsd = json.load(jsf)
     return jsd
-
-def load_history(hist_path : str) -> dict:
-    with open(hist_path, 'r') as hf:
-        hd = json.load(hf)
-    # todo - data cleaning
-    return hd
-
-def list_to_numpy(dct : dict) -> dict:
-    nd = {}
-    for k, v in dct.items():
-        if isinstance(v, dict):
-            list_to_numpy(v)
-        if isinstance(v, list):
-            v = np.asarray(v)
-        nd[k] = v
-    return nd
 
 # Jsonify numpy arrays
 # https://stackoverflow.com/questions/26646362/numpy-array-is-not-json-serializable
@@ -183,72 +137,20 @@ class NumpyEncoder(json.JSONEncoder):
     
     """
         restoring arrays - needs prior knowledge of what was array! - see https://stackoverflow.com/a/47626762/8888097
-        -> store as additional hidden config file?
     """
 
-class NumpyEncoderNice(json.JSONEncoder):
-
-    def default(self, o: Any) -> Any:
-        """
-            Input objects of ndarray will be converted to dict with dtype, shape and data in base64
-        """
-        if isinstance(o, np.ndarray):
-            obj_data = np.ascontiguousarray(o).data
-            data_b64 = base64.b64encode(obj_data)
-            return dict(__ndarray__=data_b64, dtype=str(o.dtype), shape=o.shape)
-        
-        super().default(o)
-
-def json_list_np_obj_hook(dct):
-    if isinstance(dct, list):
-        dct = np.asarray(dct)
-    return dct
-
-def json_np_obj_hook(dct):
-    """
-        Decodes np array - json combo encoded through previous thing
-    """
-    if isinstance(dct, dict) and "__ndarray__" in dct:
-        data = base64.b64decode(dct["__ndarray__"])
-        return np.frombuffer(data, dct['dtype']).reshape(dct['shape'])
-    return dct
-
-
-# working with hdf5s!
-def dump_h5(nt : list, dirname : str) -> None:
-    """
-        similar to the function for dumping namedtuples below
-    """
+def dump_pickle(nt : list, dirname : str, name="EKFlog") -> None:
     outdict = {h.t : h._asdict() for h in nt}
     
-    # the name is the name of the history
-    hname = type(nt[0]).__name__
-
     if not os.path.isdir(dirname):
         print("{} does not exist. Creating".format(dirname))
         _create_dir(dirname)
     
     # save the dictionary
-    outf = os.path.join(dirname, hname + ".hdf5")
-    with h5py.File(outf, "w") as outfile:
-        json.dump(outdict, outfile, cls=NumpyEncoderNice)
-    print("Written {} to {}".format(hname, outf))
-
-def dump_pickle(nt : list, dirname : str) -> None:
-    outdict = {h.t : h._asdict() for h in nt}
-    
-    # the name is the name of the history
-    hname = type(nt[0]).__name__
-
-    if not os.path.isdir(dirname):
-        print("{} does not exist. Creating".format(dirname))
-        _create_dir(dirname)
-    
-    # save the dictionary
-    outf = os.path.join(dirname, hname + ".pkl")
+    outf = os.path.join(dirname, name + ".pkl")
     with open(outf, "wb") as outfile:
         pickle.dump(outdict, outfile)
-    print("Written {} to {}".format(hname, outf))
+    print("Written {} to {}".format(name, outf))
 
 def load_pickle(fp : str):
     with open(fp, 'rb') as f:
