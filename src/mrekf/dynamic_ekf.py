ab850e369366883668ea4ca00313cfc7fea5f63e
Nico Mandel <mandel.nicolas@gmail.com>
2023-09-01 15:57:26 +0200

    added function for splitting state into dyn and stat through sets

--- src/mrekf/dynamic_ekf.py
+++ src/mrekf/dynamic_ekf.py
@@ -1,10 +1,7 @@
 import numpy as np
 from roboticstoolbox.mobile import VehicleBase
 from mrekf.ekf_base import BasicEKF, MR_EKFLOG
-from mrekf.ekf_math import VehicleBase
 from mrekf.motionmodels import BaseModel
-
-
 
 class Dynamic_EKF(BasicEKF):
 
@@ -13,14 +10,45 @@ class Dynamic_EKF(BasicEKF):
         self._dynamic_ids = dynamic_ids
         self._motion_model = motion_model
 
+        # overwriting history keeping!
+        if history:
+            self._htuple = MR_EKFLOG
+            self._history = []
+
     @property
-    def dynamic_idcs(self) -> list:
+    def dynamic_idcs(self) -> set:
         return self._dynamic_ids
-    
+
+    @property
+    def seen_dyn_lms(self) -> set:
+        return set(self.landmarks.keys()).intersection(set(self.dynamic_idcs))
+
+    @property
+    def seen_static_lms(self) -> set:
+        return set(self.landmarks.keys()) - set(self.dynamic_idcs)
+
     @property
     def motion_model(self) -> list:
         return self._motion_model
     
-    
-    
+    @property
+    def state_length(self):
+        """
+            Todo - could change this by storing at runtime whether somethings is dynamic or not
+        """
+        l = 3 + 2 * len(self.seen_dyn_lms) + self.motion_model.state_length * len(self.seen_dyn_lms)
+        return l
 
+    def store_history(self, t: float, x_est: np.ndarray, P_est: np.ndarray, z: dict, innov: np.ndarray, K: np.ndarray, landmarks: dict) -> None:
+        """
+        """
+        if self._keep_history:
+            hist = self._htuple(
+                # todo insert values we are storing here!
+            )
+
+    def _get_Fx(self, x_est: np.ndarray, odo) -> np.ndarray:
+        Fx = super()._get_Fx(x_est, odo)
+        mmsl = self.motion_model.state_length
+        for r in self.seen_dyn_lms:
+            
