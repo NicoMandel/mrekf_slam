fix column dimensionality of V and Fv calculation for models

src/mrekf/dynamic_ekf.py
@@ -108,18 +108,23 @@ class Dynamic_EKF(BasicEKF):
         for dlm in self.seen_dyn_lms or []:
             d_ind = self.landmark_index(dlm)
             xd = x_est[d_ind : d_ind + mmsl]
+            
+            # row corresponds to the index in the state vector
             ins_r = d_ind
-            ins_c = d_ind - 1
-            Fv[ins_r : ins_r + mmsl, ins_c : ins_c + mmsl] = self.motion_model.Fv(xd)
+            
+            # column where to insert is 2 * (1 + number of landmarks seen before)
+            lm_pos = self.landmarks[dlm][0]
+            ins_c = 2 * (1+ lm_pos)
+            Fv[ins_r : ins_r + mmsl, ins_c : ins_c + 2] = self.motion_model.Fv(xd)
         
         return Fv
     
     def _get_V(self, x_est: np.ndarray) -> np.ndarray:
         Vm =  super()._get_V(x_est)
-        mmsl = self.motion_model.state_length
+        # mmsl = self.motion_model.state_length
         for dlm in self.seen_dyn_lms or []:
             d_ind = self.landmark_index(dlm) - 1
-            Vm[d_ind : d_ind + mmsl, d_ind : d_ind + mmsl] = self.motion_model.V
+            Vm[d_ind : d_ind + 2, d_ind : d_ind + 2] = self.motion_model.V
         
         return Vm
     

src/mrekf/ekf_base.py
@@ -267,11 +267,12 @@ class BasicEKF(object):
         """
             Jacobian of f wrt to the noise v
         """
-
         xv_est = x_est[:3]
+        cols = 2 * (1 + len(self.landmarks))
+        rows = len(x_est)
+        Fv = np.zeros((rows, cols))
         Fvv = self.robot.Fv(xv_est, odo)
-        Fvm = np.eye(x_est.size - 3)
-        Fv = block_diag(Fvv, Fvm)
+        Fv[:3,:2] = Fvv
         return Fv
 
     def _get_V(self, x_est : np.ndarray) -> np.ndarray:
@@ -280,7 +281,8 @@ class BasicEKF(object):
             overwrite - for binary bayes filter in the overwritten version make the V a property of each LM -> already scaled.
                 LM can be objects that have an id, a map index, a counter + the V
         """
-        dim = len(x_est) - 1
+        dim = (1  + len(self.landmarks)) * 2
+        # dim = len(x_est) - 1
         Vm = np.zeros((dim, dim))
         V_v = self.V_est        # todo - this is different than in the OG implementation -double check if results are equivalent
         Vm[:2, :2] = V_v

src/mrekf/motionmodels.py
@@ -4,9 +4,7 @@ from abc import ABC, abstractmethod
 from spatialmath import base
 
 class BaseModel(ABC):
-    """
-        TODO: write __repr__similar to the sensor class.
-        
+    """        
     """
 
     @abstractmethod
@@ -16,6 +14,17 @@ class BaseModel(ABC):
         self._Fx = Fx
         self._state_length = state_length
     
+    def __str__(self):
+        # s = super().__str__()
+        s = f"{self.__class__.__name__} motion model\n"
+        s += f"\n  V = {base.array2str(self.V)}\n"
+        if hasattr(self, "dt"):
+            s += f"  dt: ({self.dt})\n"
+        return s.rstrip()
+
+    def __repr__(self) -> str:
+        return str(self)
+
     # Motion Models
     @abstractmethod
     def f(self, x : np.ndarray) -> np.ndarray:
@@ -74,7 +83,7 @@ class KinematicModel(BaseModel):
     """
 
     def __init__(self, V : np.ndarray, dt : float, vmax : float = 3.0) -> None:
-        assert V.shape == (4,4), "V not correct shape, Please make sure it's 4x4"
+        assert V.shape == (2,2), "V not correct shape, Please make sure it's 2x2"
         dim = 4
 
         # derivative Matrices
@@ -86,10 +95,10 @@ class KinematicModel(BaseModel):
         ])
 
         Fv = np.array([
-            [0., 0., dt, 0.],
-            [0., 0., 0., dt],
-            [0., 0., 1., 0.],
-            [0., 0., 0., 1.],
+            [dt, 0.],
+            [0., dt],
+            [1., 0.],
+            [0., 1.],
         ])
         super().__init__(V, Fv, Fx, dim)
         self._dt = dt
@@ -115,7 +124,6 @@ class KinematicModel(BaseModel):
     def A(self) -> np.ndarray:
         return self._A
 
-
     def f(self, x : np.ndarray) -> np.ndarray:
         fx_k = self.A @ x
         return fx_k   
@@ -132,8 +140,7 @@ class BodyFrame(BaseModel):
     """
 
     def __init__(self, V : np.ndarray, dt : float, vmax : float = 3.0):
-        assert V.shape == (4,4), "V not correct shape, Please make sure it's 4x4"
-        assert np.allclose(V[:2,:2], np.zeros((2,2))), "The top left corner of V should be all zeros, please correct"
+        assert V.shape == (2,2), "V not correct shape, Please make sure it's 2x2"
         dim = 4
 
         Fv = None
@@ -168,24 +175,24 @@ class BodyFrame(BaseModel):
             Since these are nonlinear models, the update equations are different.
             # todo check angle wrapping!
         """
-        cos_x3 = np.cos(base.wrap_mpi_pi(x[3] + self.V[3,3]))
-        sin_x3 = np.sin(base.wrap_mpi_pi(x[3] + self.V[3,3]))
+        cos_x3 = np.cos(base.wrap_mpi_pi(x[3] + self.V[1,1]))
+        sin_x3 = np.sin(base.wrap_mpi_pi(x[3] + self.V[1,1]))
         fx = np.array([
-            [1., 0., self.dt * cos_x3,  -1. * self.dt * (x[2] + self.V[2,2]) * sin_x3],
-            [0., 1., self.dt * sin_x3,   self.dt * (x[2] + self.V[2,2]) * cos_x3],
+            [1., 0., self.dt * cos_x3,  -1. * self.dt * (x[2] + self.V[0,0]) * sin_x3],
+            [0., 1., self.dt * sin_x3,   self.dt * (x[2] + self.V[0,0]) * cos_x3],
             [0., 0., 1., 0.],
             [0., 0., 0., 1.]
             ])
         return fx
 
     def Fv(self, x: np.ndarray) -> np.ndarray:
-        cos_x3 = np.cos(base.wrap_mpi_pi(x[3] + self.V[3,3]))
-        sin_x3 = np.sin(base.wrap_mpi_pi(x[3] + self.V[3,3]))
+        cos_x3 = np.cos(base.wrap_mpi_pi(x[3] + self.V[1,1]))
+        sin_x3 = np.sin(base.wrap_mpi_pi(x[3] + self.V[1,1]))
         fv = np.array([
-            [0., 0., self.dt * cos_x3,  -1. * self.dt * (x[2] + self.V[2,2]) * sin_x3],
-            [0., 0., self.dt * sin_x3,  self.dt * (x[2] + self.V[2,2]) * cos_x3],
-            [0., 0., 1., 0.],
-            [0., 0., 0., 1.]
+            [self.dt * cos_x3,  -1. * self.dt * (x[2] + self.V[0,0]) * sin_x3],
+            [self.dt * sin_x3,  self.dt * (x[2] + self.V[0,0]) * cos_x3],
+            [1., 0.],
+            [0., 1.]
             ])
         return fv
 