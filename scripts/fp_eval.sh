73d827b34a5903c0c7db5a0eec03e92eb1f5aeea
Nico Mandel <mandel.nicolas@gmail.com>
2023-12-30 11:23:55 +0100

    removed dyn lms from fp eval - not necessary - reduce count

--- scripts/fp_eval.sh
+++ scripts/fp_eval.sh
@@ -8,12 +8,9 @@ do
     rp=$(realpath $cf)
     for s in {2..30..1}
     do
-        for d in {1..5..1}                          
-        do    
-            for sd in {5..105}    # 105
-            do
-                python arguments.py -d $d -s $s --seed $sd --config $rp -c $csvf --dynamicfilter --incfilter
-            done        
-        done
+        for sd in {5..105}    # 105
+        do
+            python arguments.py -d 1 -s $s --seed $sd --config $rp -c $csvf --dynamicfilter --incfilter
+        done        
     done
 done
