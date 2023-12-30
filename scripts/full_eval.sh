fbb78066bc6b2558f59e7131c6e111332a017b71
Nico Mandel <mandel.nicolas@gmail.com>
2023-12-30 11:28:55 +0100

    added full evaluation scriot

---
+++ scripts/full_eval.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CONFFILES=$(find ../config/*.yaml -type f)
+csvf="full_eval"
+
+for cf in $CONFFILES
+do
+    rp=$(realpath $cf)
+    for s in {2..30..1}
+    do
+        for d in {1..5..1}                          
+        do 
+            for sd in {5..105}    # 105
+            do
+                python arguments.py -d $d -s $s --seed $sd --config $rp -c $csvf
+            done        
+        done
+    done
+done
