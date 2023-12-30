37715b6689ee2e3260972e69b3fe228fc1ddb6a0
Nico Mandel <mandel.nicolas@gmail.com>
2023-12-30 11:00:55 +0100

    added script for fp eval and fixed motionmodel from fn eval

--- scripts/fn_eval.sh
+++ scripts/fn_eval.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-CONFFILES=$(find ../config/*.yaml -type f)
+
 csvf="false_negative"
 
-for cf in $CONFFILES
+for s in {2..30..1}
 do
-    rp=$(realpath $cf)
-    for s in {2..30..1}
-    do
-        for d in {1..5..1}                          
-        do    
-            for sd in {5..105}    # 105
-            do
-                python arguments.py -d $d -s $s --seed $sd --config $rp -c $csvf --fpfilter --dynamicfilter
-            done        
-        done
+    for d in {1..5..1}                          
+    do    
+        for sd in {5..105}    # 105
+        do
+            python arguments.py -d $d -s $s --seed $sd -c $csvf --fpfilter --dynamicfilter
+        done        
     done
 done
