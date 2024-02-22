8d1583547a96a15d550750cb405d50c683e07d15
Nico Mandel <mandel.nicolas@gmail.com>
2024-02-22 21:58:26 +0100

    GH-17, GH-14 - new calculation of ate from loaded histories

--- scripts/plot_case.py
+++ scripts/plot_case.py
@@ -1,18 +1,20 @@
 """
     File to plot a specific case
 """
-import os.path
+import os
 from argparse import ArgumentParser
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm import tqdm
 
 from roboticstoolbox import LandmarkMap
 
 from mrekf.utils import load_json, load_exp_from_csv, load_histories_from_dir, load_gt_from_dir
 from mrekf.eval_utils import  get_ignore_idcs, get_transform, has_dynamic_lms,\
                             plot_gt, plot_xy_est, plot_dyn_gt, plot_dyn_est, plot_transformed_xy_est,\
-                            get_transform_offsets
+                            get_transform_offsets, _get_xyt_true, get_ATE
 
 def parse_args(defdir : str):
     """
@@ -27,9 +29,69 @@ def parse_args(defdir : str):
     parser.add_argument("-n", "--name", type=str, default=default_case, help="Name of the file / folder combination to look for in the input directory")
     parser.add_argument("-d", "--directory", type=str, default=defdir, help="Name of the default directory to look for")
     parser.add_argument("-e", "--experiment", default=defexp, type=str, help="Name of the experiment, named like the csv file where to look for the case")
-
     args = vars(parser.parse_args())
     return args
+
+def recalculate(directory : str, experiment : str, csvfn : str):
+    pdir = os.path.join(directory, experiment)
+    # for every subdirectory in the pdir
+    list_subfolders_with_paths = [f.name for f in os.scandir(pdir) if f.is_dir()]
+    csv_f = os.path.join(directory, csvfn)
+    # load the json
+    for key in tqdm(list_subfolders_with_paths):
+        jsonf = os.path.join(pdir, key + ".json")
+        simdict = load_json(jsonf)
+
+        # load the experiments
+        rdir = os.path.join(pdir, key)
+        ekf_hists = load_histories_from_dir(rdir)
+        gt_hist = load_gt_from_dir(rdir)
+        csv_row_df = recalc_case(simdict, ekf_hists, gt_hist, key)
+
+        # Writign to csv file
+        with open(csv_f, 'a') as cf:
+            csv_row_df.to_csv(cf, mode="a", header=cf.tell()==0)
+
+    # append the new results to the csvfile-new, which should live in "directory"
+    print("Test Debug line")
+
+def recalc_case(simdict : dict, ekf_hists : dict, gt_hist : dict, identity : str) -> pd.DataFrame:
+    # Calculate ATE
+    workspace = np.array(simdict['map']['workspace'])
+    mp = np.array(simdict['map']['landmarks'])
+    lm_map = LandmarkMap(map=mp, workspace=workspace)
+    x_true = _get_xyt_true(gt_hist)
+
+    ate_d = {
+        # "timestamp" : outname,
+        "dynamic" : len(simdict["dynamic"]),
+        "static" : lm_map._nlandmarks,
+        "time" : len(gt_hist) / 10,             # ! kinda - hardcoded
+        "seed" : simdict["seed"],
+        "fp_count" : 1,                             # ! hardcoded!
+        # "motion_model" : simdict['motion_model']['type']
+    }
+    for ekf_id, ekf_hist in ekf_hists.items():
+        cfg_ekf = simdict[ekf_id]
+        ign_idcs = get_ignore_idcs(cfg_ekf, simdict)
+        ate, R_e, t_e =  get_ATE(
+            hist = ekf_hist,
+            map_lms = lm_map,
+            x_t = x_true,
+            ignore_idcs = ign_idcs 
+            )
+        
+        ate_d[ekf_id + "-ate"] = np.sqrt(ate.mean())
+
+        # get transformation parameters
+        t_d, R_d  = get_transform_offsets(t_e, R_e)
+        ate_d[ekf_id + "-translation_dist"] = t_d
+        ate_d[ekf_id + "-rotation_dist"] = R_d
+    df = pd.DataFrame(
+        data=ate_d,
+        index=[identity]
+    )
+    return df
 
 def filter_dict(in_dict : dict, *inkey : list) -> dict:
     return {k:v for ik in inkey for k,v in in_dict.items() if ik in k}
@@ -42,6 +104,9 @@ if __name__=="__main__":
     directory = args["directory"]
     experiment = args["experiment"]
     name = args["name"]
+
+    recalculate(directory, experiment, "newfile.csv")
+
     # loading experiment values from csv
     csvf = os.path.join(directory, experiment + ".csv")
     exp_res = load_exp_from_csv(csvf, name)
